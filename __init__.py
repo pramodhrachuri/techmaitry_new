b201404bd233d661b859117d9c4c1bf848a1ada0
Pramodh <pramodhrachuri@gmail.com>
2017-11-05 19:31:32 +0530

    LOT of changes

--- __init__.py
+++ __init__.py
@@ -10,8 +10,22 @@ app.config['SECRET_KEY'] = 'my details secret!'
 
 
 @app.route('/',methods=['GET'])
-def potato():
+def index():
 	return render_template('index.html')
+
+@app.route('/form',methods=['GET','POST'])
+def form():
+	return render_template('form.html')
+
+@app.route('/form_main',methods=['GET','POST'])
+def form_main():
+	return render_template('form_main.html')
+	
+@app.route('/form_submit',methods=['GET','POST'])
+def form_submit():
+	a = request.form['form-contact-name']
+	print a
+	return "hi"
 
 @app.route('/form_data',methods=['POST','GET'])
 def form_data():
